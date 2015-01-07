1acb3e1b17fa573e502ff2bbde6d5c0b726f3460
JerryDing2014 <jerryd221@gmail.com>
2015-01-07 21:55:04 +1100

    fix the namespace

--- src/AppBundle/Response/ErrorListResponse.php
+++ src/AppBundle/Response/ErrorListResponse.php
@@ -6,7 +6,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Component\Response;
+namespace AppBundle\Response;
 
 class ErrorListResponse
 {
--- src/AppBundle/Response/JsonResponse.php
+++ src/AppBundle/Response/JsonResponse.php
@@ -6,8 +6,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Component\Response;
-
+namespace AppBundle\Response;
 
 class JsonResponse extends Response
 {
