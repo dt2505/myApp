aa30c94008e36e3cfb3d71eecb419322a805f69e
JerryDing2014 <jerryd221@gmail.com>
2015-01-17 18:08:36 +1100

    Add UserController to handle user's behavior

---
+++ src/SecretBase/AppBundle/Controller/UserController.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * This file is Copyright (c).
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SecretBase\AppBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends ImageController
+{
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @Rest\Post()
+     */
+    public function registerAction(Request $request)
+    {
+        return new JsonResponse(array("message" => "for user registration", "code" => 200));
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @Rest\Post()
+     */
+    public function uploadAvatarAction(Request $request)
+    {
+        return new JsonResponse(array("message" => "for uploading user's avatar", "code" => 200));
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @Rest\Post()
+     */
+    public function uploadCoverAction(Request $request)
+    {
+        return new JsonResponse(array("message" => "for uploading user's cover", "code" => 200));
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @Rest\Post()
+     */
+    public function updateProfileAction(Request $request)
+    {
+        return new JsonResponse(array("message" => "for updating user's profile details", "code" => 200));
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @Rest\Post()
+     */
+    public function resetPasswordAction(Request $request)
+    {
+        return new JsonResponse(array("message" => "for resetting user's password", "code" => 200));
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @Rest\Post()
+     */
+    public function changePasswordAction(Request $request)
+    {
+        return new JsonResponse(array("message" => "for changing user's password", "code" => 200));
+    }
+}
