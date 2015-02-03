c4dc8d2bd0290a92085a379669da2979876f0902
Jerry Ding <jerry@ladoo.com.au>
2015-02-04 10:31:35 +1100

    add comment

---
+++ src/SecretBase/AppBundle/Services/Document/Comment.php
@@ -0,0 +1,112 @@
+<?php
+
+
+/**
+ * This file is Copyright (c)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SecretBase\AppBundle\Services\Document;
+
+use SecretBase\AppBundle\Entity\User;
+
+class Comment
+{
+    /** @var string */
+    private $id;
+    /** @var string */
+    private $text;
+    /** @var string */
+    private $parentId;
+    /** @var \DateTime */
+    private $createdAt;
+    /** @var User */
+    private $user;
+
+    public function __construct($id = '')
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParentId()
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @param string $parentId
+     */
+    public function setParentId($parentId)
+    {
+        $this->parentId = $parentId;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+}
+ 
--- src/SecretBase/AppBundle/Services/Manager/CommentManager.php
+++ src/SecretBase/AppBundle/Services/Manager/CommentManager.php
@@ -8,14 +8,26 @@
 
 namespace SecretBase\AppBundle\Services\Manager;
 
+use SecretBase\AppBundle\Services\Document\Comment;
 use SecretBase\AppBundle\Services\NoSQLStorage\IDocumentManager;
 
 class CommentManager extends ODManager
 {
     const ES_IDX_TYPE = "comments";
+    private $comment;
 
     public function __construct(IDocumentManager $dm)
     {
         parent::__construct($dm, self::ES_IDX_TYPE);
+    }
+
+    /**
+     * @param string $id
+     * @return Comment
+     */
+    public function create($id = '')
+    {
+        $this->comment =  new Comment($id);
+        return $this->comment;
     }
 }
