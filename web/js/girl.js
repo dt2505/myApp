c26c59adfded31b8f73a9c7b7f2a1b0b0587fe0d
JerryDing2014 <jerryd221@gmail.com>
2015-03-28 21:06:57 +1100

    add detail edit page

--- web/js/girl.js
+++ web/js/girl.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-
     // Calendar
     // =================================================================
     // Require Full Calendar
@@ -11,22 +9,22 @@ $(document).ready(function() {
 
     // initialize the external events
     // -----------------------------------------------------------------
-    $('#demo-external-events .fc-event').each(function() {
-        // store data so the calendar knows to render an event upon drop
-        $(this).data('event', {
-            title: $.trim($(this).text()), // use the element's text as the event title
-            stick: true, // maintain when user navigates (see docs on the renderEvent method)
-            className : $(this).data('class')
-        });
-
-
-        // make the event draggable using jQuery UI
-        $(this).draggable({
-            zIndex: 99999,
-            revert: true,      // will cause the event to go back to its
-            revertDuration: 0  //  original position after the drag
-        });
-    });
+    //$('#demo-external-events .fc-event').each(function() {
+    //    // store data so the calendar knows to render an event upon drop
+    //    $(this).data('event', {
+    //        title: $.trim($(this).text()), // use the element's text as the event title
+    //        stick: true, // maintain when user navigates (see docs on the renderEvent method)
+    //        className : $(this).data('class')
+    //    });
+    //
+    //
+    //    // make the event draggable using jQuery UI
+    //    $(this).draggable({
+    //        zIndex: 99999,
+    //        revert: true,      // will cause the event to go back to its
+    //        revertDuration: 0  //  original position after the drag
+    //    });
+    //});
 
     $opt = {
         header: {
@@ -43,7 +41,28 @@ $(document).ready(function() {
                 $(this).remove();
             }
         },
-        defaultDate: '2015-01-12',
+        dayClick: function(date, jsEvent, view) {
+
+            alert('Clicked on: ' + date.format());
+
+            alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+
+            alert('Current view: ' + view.name);
+
+            // change the day's background color just for fun
+            $(this).css('background-color', 'red');
+        },
+        eventClick: function(calEvent, jsEvent, view) {
+
+            alert('Event: ' + calEvent.title);
+            alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+            alert('View: ' + view.name);
+
+            // change the border color just for fun
+            $(this).css('border-color', 'red');
+
+        },
+        defaultDate: '2015-01-1',
         eventLimit: true, // allow "more" link when too many events
         events: [
             {
@@ -130,23 +149,145 @@ $(document).ready(function() {
             }
         ]
     };
+
     // Initialize the calendar
     // -----------------------------------------------------------------
-    $('#demo-calendar').fullCalendar($opt);
+    $('#calendar').fullCalendar($opt);
+
+    // Initialize chosen
+    // -----------------------------------------------------------------
     $('#acceptable-clients').chosen({width:'100%'});
 
-    // PANEL WITH BUTTONS - LOADING OVERLAY
-    $('.panel-ref-btn').niftyOverlay().on('click', function(){
-        var $el = $(this), relTime;
-        $el.niftyOverlay('show');
+    // Initialize click event
+    // -----------------------------------------------------------------
+    var $servicesPanelGroup = $('#services-panel-group'),
+        $serviceCurrencyUl = $servicesPanelGroup.find(".up-currency-dropdown-menu"),
+        $unitUl = $servicesPanelGroup.find(".up-unit-dropdown-menu"),
+        $optionsPanelGroup = $('#acceptable-opt-panel-group'),
+        $optionCurrencyUl = $optionsPanelGroup.find(".up-currency-dropdown-menu"),
+        onCurrencyClick = function() {
+            var $self = $(this),
+                selectedText = $self.data("short-name"),
+                selectedSymbol = $self.data("symbol"),
+                selectedId = $self.data("id"),
+                currencyId = $self.data("currency-id"),
+                $inputGrpBtn = $("#input-grp-" + currencyId),
+                $hiddeninput = $('#hidden-input-' + currencyId),
+                $btnDropDownCurrency = $('#btn-dropdown-' + currencyId),
+                currentCurrency = $btnDropDownCurrency.text().trim();
 
-        // Do something...
+            $btnDropDownCurrency.html($btnDropDownCurrency.html().trim().replace(currentCurrency, selectedText + selectedSymbol));
+            $hiddeninput.val(selectedId);
 
-        relTime = setInterval(function(){
-            // Hide the screen overlay
-            $el.niftyOverlay('hide');
+            $self.parent().parent().find("li.active").removeClass("active");
+            $self.parent().addClass("active");
+            $inputGrpBtn.toggleClass("open");
 
-            clearInterval(relTime);
-        },2000);
+            return false;
+        };
+
+    // currency dropdown click
+    $optionCurrencyUl.find("li a").on("click", onCurrencyClick);
+    $serviceCurrencyUl.find("li a").on("click", onCurrencyClick);
+
+    // unit dropdown click
+    $unitUl.find("li a").on("click", function(){
+        var $self = $(this),
+            selectedText = $self.data("name"),
+            selectedId = $self.data("id"),
+            unitId = $self.data("unit-id"),
+            $inputGrpBtn = $("#input-grp-" + unitId),
+            $hiddeninput = $('#hidden-input-' + unitId),
+            $btnDropDownCurrency = $('#btn-dropdown-' + unitId),
+            currentUnit = $btnDropDownCurrency.text().trim();
+
+        $btnDropDownCurrency.html($btnDropDownCurrency.html().trim().replace(currentUnit, selectedText));
+        $hiddeninput.val(selectedId);
+
+        $unitUl.find("li.active").removeClass("active");
+        $(this).parent().addClass("active");
+        $inputGrpBtn.toggleClass("open");
+
+        return false;
+    });
+
+    // save btn click - loading overlay
+    var $itemSaveBtn = $('.item-save'),
+        overlay = function($el, process) {
+            var relTime;
+            $el.niftyOverlay('show');
+
+            process($el);
+
+            relTime = setInterval(function(){
+                // Hide the screen overlay
+                $el.niftyOverlay('hide');
+
+                clearInterval(relTime);
+            }, 2000);
+        },
+        getItems = function ($itemBody) {
+            var items = [];
+            $itemBody.find("input[type=checkbox]").each(function(index){
+                var $el = $(this),
+                    itemId = $el.data("item-id"),
+                    prefix = $el.data("prefix"),
+                    hasUnit = $el.data("has-unit"),
+                    hasDesc = $el.data("has-desc"),
+                    unitValue = hasUnit ? $('#' + prefix + "-input-unit-value-" + itemId).val() : null,
+                    selectedUnit = hasUnit ? $('#' + prefix + "-input-selected-unit-" + itemId).val() : null,
+                    price = $('#' + prefix + "-input-price-" + itemId).val(),
+                    selectedCurrency = $('#' + prefix + "-input-selected-currency-" + itemId).val(),
+                    desc = hasDesc ? $('#' + prefix + "-textarea-desc-" + itemId).val() : null;
+
+                items[index] = {
+                    id: itemId,
+                    unit: {
+                        value: unitValue,
+                        selected: selectedUnit
+                    },
+                    price: {
+                        value: price,
+                        currency: selectedCurrency
+                    },
+                    desc: desc
+                }
+            });
+
+            return items;
+        };
+
+    // save item details(services and options)
+    $itemSaveBtn.niftyOverlay().on('click', function(){
+        var items;
+
+        overlay($(this), function($el) {
+            items = getItems($($el.data("item-body")));
+        });
+
+        console.log(items);
+        //TODO: make an ajax call to server for persisting data
+    });
+
+    // save all services and options
+    $(".service-save, .option-save").niftyOverlay().on('click', function(){
+
+        overlay($(this), function ($el) {
+            var $target = $($el.data("target")),
+                itemIdentifier = $el.data("item-identifier"),
+                items = [];
+
+            $target.find("." + itemIdentifier + "-item-checkbox").each(function(index){
+                var $el = $(this),
+                    $itemBody = $($el.data("item-body")),
+                    $itemId = $el.data("item-id"),
+                    $itemName = $el.data("item-name");
+
+                items[index] = {id: $itemId, name: $itemName, items: getItems($itemBody)};
+            });
+
+            console.log(items);
+            //TODO: make an ajax call to server for persisting data
+        });
     });
 });
