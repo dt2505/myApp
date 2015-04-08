62e818bf5334b0ff3eb80ebf24859b0c0f72bfd5
JerryDing2014 <jerryd221@gmail.com>
2015-04-08 22:06:17 +1000

    re-organize css and js files - final step

--- src/SecretBase/AppBundle/AppBundle.php
+++ src/SecretBase/AppBundle/AppBundle.php
@@ -2,10 +2,7 @@
 
 namespace SecretBase\AppBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-use SecretBase\AppBundle\DependencyInjection\Compiler\StorageProviderCompilerPass;
 
 class AppBundle extends Bundle
 {
